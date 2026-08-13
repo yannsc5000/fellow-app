d0147af8c59e14f423c96b1c8cc5e85b535257bd
yannsc5000 <yannsc@gmail.com>
2026-08-12 23:35:18 -0400

    Wire next-intl plugin in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -14,4 +18,4 @@ const nextConfig = {
     return [{ source: "/sitemap", destination: "/sitemap.xml", permanent: true }];
   },
 };
-export default nextConfig;
+export default withNextIntl(nextConfig);
